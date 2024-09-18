1616032a071d9b7da5b47786ebbc71c8c8cd8f6e
fajri-farid <fajrijry14@gmail.com>
2024-09-18 14:48:07 +0800

    header done

--- src/component/organism/header.jsx
+++ src/component/organism/header.jsx
@@ -5,10 +5,14 @@ import Input from "../atoms/input";
 function Header() {
   return (
     <div className="bg-green-400 w-full p-4">
-      <div className="flex items-center gap-8">
-        <Link to="/" className="text-black text-xl font-bold">
-          TokoOnline
-        </Link>
+      <div className="flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-8">
+        <div className="flex justify-between w-full sm:w-auto">
+          <Link to="/" className="text-black text-xl font-bold">
+            TokoOnline
+          </Link>
+          {/* Ini akan disembunyikan pada layar 'md' dan lebih besar */}
+          <Button label="login" className="md:hidden" />
+        </div>
         <div className="relative flex-grow">
           <Input
             type="search"
@@ -18,7 +22,10 @@ function Header() {
           />
           <i className="fa-solid fa-magnifying-glass absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
         </div>
-        <Button label="login" />
+        {/* Ini akan tampil di layar 'md' dan lebih besar */}
+        <div className="hidden md:block">
+          <Button label="login" />
+        </div>
       </div>
     </div>
   );
